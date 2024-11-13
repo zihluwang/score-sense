97f4c12a7095920d3bc5483e4dd8ede55741af75
zihluwang <really@zihlu.wang>
2024-11-14 00:14:12 +0800

    fix: 修复从微信获取小程序分享码并存储后文件名称错误的问题

--- score-sense-db/dbinit.d/01_model.sql
+++ score-sense-db/dbinit.d/01_model.sql
@@ -1,13 +1,15 @@
-CREATE TABLE "answer" (
-  "exam_id" int8 NOT NULL,
-  "question_id" int8 NOT NULL,
-  "user_id" int8 NOT NULL,
-  "answer_text" text COLLATE "pg_catalog"."default" NOT NULL,
-  "submitted_at" timestamp(6) NOT NULL DEFAULT now(),
-  "score" int4 NOT NULL DEFAULT 0,
-  CONSTRAINT "answer_pkey" PRIMARY KEY ("exam_id", "question_id", "user_id")
+CREATE TABLE "answer"
+(
+    "exam_id"      int8                                NOT NULL,
+    "question_id"  int8                                NOT NULL,
+    "user_id"      int8                                NOT NULL,
+    "answer_text"  text COLLATE "pg_catalog"."default" NOT NULL,
+    "submitted_at" timestamp(6)                        NOT NULL DEFAULT now(),
+    "score"        int4                                NOT NULL DEFAULT 0,
+    CONSTRAINT "answer_pkey" PRIMARY KEY ("exam_id", "question_id", "user_id")
 );
-ALTER TABLE "answer" OWNER TO "postgres";
+ALTER TABLE "answer"
+    OWNER TO "postgres";
 COMMENT ON COLUMN "answer"."exam_id" IS '考试 ID';
 COMMENT ON COLUMN "answer"."question_id" IS '试题 ID';
 COMMENT ON COLUMN "answer"."user_id" IS '用户 ID';
@@ -15,31 +17,34 @@ COMMENT ON COLUMN "answer"."answer_text" IS '答案';
 COMMENT ON COLUMN "answer"."submitted_at" IS '提交时间';
 COMMENT ON COLUMN "answer"."score" IS '用户在该题获得的分数，将实际成绩 * 100 存储，如 100 分存储为 10,000。';
 
-CREATE TABLE "attachment" (
-  "id" int8 NOT NULL,
-  "name" varchar(255) NOT NULL,
-  "path" varchar(255) NOT NULL,
-  "content_type" varchar(255) NOT NULL,
-  PRIMARY KEY ("id")
+CREATE TABLE "attachment"
+(
+    "id"           int8         NOT NULL,
+    "name"         varchar(255) NOT NULL,
+    "path"         varchar(255) NOT NULL,
+    "content_type" varchar(255) NOT NULL,
+    PRIMARY KEY ("id")
 );
 COMMENT ON COLUMN "attachment"."id" IS '附件 ID';
 COMMENT ON COLUMN "attachment"."name" IS '附件名称';
 COMMENT ON COLUMN "attachment"."path" IS '文件在服务器本地的存储路径';
 COMMENT ON COLUMN "attachment"."content_type" IS '文件类型';
 
-CREATE TABLE "exam" (
-  "id" int8 NOT NULL,
-  "name" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
-  "type" int4 NOT NULL,
-  "description" text COLLATE "pg_catalog"."default",
-  "province" char(2) NOT NULL,
-  "prefecture" char(4) NOT NULL,
-  "base_num" int4 NOT NULL DEFAULT 0,
-  "released_at" timestamp NOT NULL,
-  "status" int4 NOT NULL,
-  CONSTRAINT "exam_pkey" PRIMARY KEY ("id")
+CREATE TABLE "exam"
+(
+    "id"          int8                                        NOT NULL,
+    "name"        varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
+    "type"        int4                                        NOT NULL,
+    "description" text COLLATE "pg_catalog"."default",
+    "province"    char(2)                                     NOT NULL,
+    "prefecture"  char(4)                                     NOT NULL,
+    "base_num"    int4                                        NOT NULL DEFAULT 0,
+    "released_at" timestamp                                   NOT NULL,
+    "status"      int4                                        NOT NULL,
+    CONSTRAINT "exam_pkey" PRIMARY KEY ("id")
 );
-ALTER TABLE "exam" OWNER TO "postgres";
+ALTER TABLE "exam"
+    OWNER TO "postgres";
 COMMENT ON COLUMN "exam"."id" IS '考试 ID';
 COMMENT ON COLUMN "exam"."name" IS '考试名称';
 COMMENT ON COLUMN "exam"."type" IS '考试类型';
@@ -51,120 +56,136 @@ COMMENT ON COLUMN "exam"."released_at" IS '考试发布时间';
 COMMENT ON COLUMN "exam"."status" IS '考试状态';
 COMMENT ON TABLE "exam" IS '考试信息';
 
-CREATE TABLE "exam_result" (
-  "exam_id" int8 NOT NULL,
-  "user_id" int8 NOT NULL,
-  "vacancy_id" int8 NOT NULL,
-  "score" int4 NOT NULL DEFAULT 0,
-  "completed_at" timestamp(6),
-  CONSTRAINT "exam_result_pkey" PRIMARY KEY ("exam_id", "user_id")
+CREATE TABLE "exam_result"
+(
+    "exam_id"      int8 NOT NULL,
+    "user_id"      int8 NOT NULL,
+    "vacancy_id"   int8 NOT NULL,
+    "score"        int4 NOT NULL DEFAULT 0,
+    "completed_at" timestamp(6),
+    CONSTRAINT "exam_result_pkey" PRIMARY KEY ("exam_id", "user_id")
 );
-ALTER TABLE "exam_result" OWNER TO "postgres";
+ALTER TABLE "exam_result"
+    OWNER TO "postgres";
 COMMENT ON COLUMN "exam_result"."exam_id" IS '考试 ID';
 COMMENT ON COLUMN "exam_result"."user_id" IS '参考用户 ID';
 COMMENT ON COLUMN "exam_result"."vacancy_id" IS '岗位 ID';
 COMMENT ON COLUMN "exam_result"."score" IS '总成绩';
 COMMENT ON COLUMN "exam_result"."completed_at" IS '完成考试时间';
 
-CREATE TABLE "exam_type" (
-  "id" serial,
-  "name" varchar(255) NOT NULL,
-  PRIMARY KEY ("id")
+CREATE TABLE "exam_type"
+(
+    "id"   serial,
+    "name" varchar(255) NOT NULL,
+    PRIMARY KEY ("id")
 );
 COMMENT ON COLUMN "exam_type"."id" IS '考试类型 ID';
 COMMENT ON COLUMN "exam_type"."name" IS '考试类型名称';
 
-CREATE TABLE "exam_vacancy" (
-  "exam_id" int8 NOT NULL,
-  "vacancy_id" int8 NOT NULL,
-  PRIMARY KEY ("exam_id", "vacancy_id")
+CREATE TABLE "exam_vacancy"
+(
+    "exam_id"    int8 NOT NULL,
+    "vacancy_id" int8 NOT NULL,
+    PRIMARY KEY ("exam_id", "vacancy_id")
 );
 COMMENT ON COLUMN "exam_vacancy"."exam_id" IS '考试 ID';
 COMMENT ON COLUMN "exam_vacancy"."vacancy_id" IS '岗位 ID';
 
-CREATE TABLE "option" (
-  "exam_id" int8 NOT NULL,
-  "question_id" int8 NOT NULL,
-  "id" char(1) COLLATE "pg_catalog"."default" NOT NULL,
-  "option_text" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
-  "is_correct" bool NOT NULL DEFAULT false,
-  CONSTRAINT "option_pkey" PRIMARY KEY ("exam_id", "question_id", "id")
+CREATE TABLE "option"
+(
+    "exam_id"     int8                                        NOT NULL,
+    "question_id" int8                                        NOT NULL,
+    "id"          char(1) COLLATE "pg_catalog"."default"      NOT NULL,
+    "option_text" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
+    "correct"     bool                                        NOT NULL DEFAULT false,
+    CONSTRAINT "option_pkey" PRIMARY KEY ("exam_id", "question_id", "id")
 );
-ALTER TABLE "option" OWNER TO "postgres";
+ALTER TABLE "option"
+    OWNER TO "postgres";
 COMMENT ON COLUMN "option"."exam_id" IS '考试 ID';
 COMMENT ON COLUMN "option"."question_id" IS '试题 ID';
 COMMENT ON COLUMN "option"."id" IS '选项 ID';
 COMMENT ON COLUMN "option"."option_text" IS '选项文本';
-COMMENT ON COLUMN "option"."is_correct" IS '是否是正确选项';
+COMMENT ON COLUMN "option".correct IS '是否是正确选项';
 
-CREATE TABLE "prefecture" (
-  "code" char(4) NOT NULL,
-  "name" varchar(255) NOT NULL,
-  "province_code" char(2) NOT NULL,
-  PRIMARY KEY ("code")
+CREATE TABLE "prefecture"
+(
+    "code"          char(4)      NOT NULL,
+    "name"          varchar(255) NOT NULL,
+    "province_code" char(2)      NOT NULL,
+    PRIMARY KEY ("code")
 );
 COMMENT ON COLUMN "prefecture"."code" IS '城市代码';
 COMMENT ON COLUMN "prefecture"."name" IS '城市名称';
 COMMENT ON COLUMN "prefecture"."province_code" IS '所属省份代码';
 
-CREATE TABLE "province" (
-  "code" char(2) NOT NULL,
-  "name" varchar(255) NOT NULL,
-  PRIMARY KEY ("code")
+CREATE TABLE "province"
+(
+    "code" char(2)      NOT NULL,
+    "name" varchar(255) NOT NULL,
+    PRIMARY KEY ("code")
 );
 COMMENT ON COLUMN "province"."code" IS '省份代码';
 COMMENT ON COLUMN "province"."name" IS '省份名称';
 
-CREATE TABLE "question" (
-  "exam_id" int8 NOT NULL,
-  "id" int8 NOT NULL,
-  "type" int4 NOT NULL,
-  "question_text" text COLLATE "pg_catalog"."default" NOT NULL,
-  "image_id" int8,
-  "max_score" int4 NOT NULL,
-  CONSTRAINT "question_pkey" PRIMARY KEY ("exam_id", "id")
+CREATE TABLE "question"
+(
+    "exam_id"       int8                                NOT NULL,
+    "id"            int8                                NOT NULL,
+    "type"          int4                                NOT NULL,
+    "answer_type"   int4                                not null,
+    "question_text" text COLLATE "pg_catalog"."default" NOT NULL,
+    "image_id"      int8,
+    "max_score"     int4                                NOT NULL,
+    CONSTRAINT "question_pkey" PRIMARY KEY ("exam_id", "id")
 );
-ALTER TABLE "question" OWNER TO "postgres";
+ALTER TABLE "question"
+    OWNER TO "postgres";
 COMMENT ON COLUMN "question"."exam_id" IS '考试 ID';
 COMMENT ON COLUMN "question"."id" IS '题目 ID';
 COMMENT ON COLUMN "question"."type" IS '试题类型';
+COMMENT ON COLUMN "question".answer_type IS '答题类型';
 COMMENT ON COLUMN "question"."question_text" IS '题干';
 COMMENT ON COLUMN "question"."image_id" IS '图片 ID';
 COMMENT ON COLUMN "question"."max_score" IS '题目满分，将实际成绩 * 100 存储，如 100 存储为 10,000';
 COMMENT ON TABLE "question" IS '试题';
 
-CREATE TABLE "question_type" (
-  "id" int4 NOT NULL,
-  "label" varchar(255) NOT NULL,
-  PRIMARY KEY ("id")
+CREATE TABLE "question_type"
+(
+    "id"    int4         NOT NULL,
+    "label" varchar(255) NOT NULL,
+    PRIMARY KEY ("id")
 );
 COMMENT ON COLUMN "question_type"."id" IS '题型 ID';
 COMMENT ON COLUMN "question_type"."label" IS '题型描述';
 
-CREATE TABLE "sequence" (
-  "key" varchar(255) NOT NULL,
-  "next" int8 NOT NULL,
-  PRIMARY KEY ("key")
+CREATE TABLE "sequence"
+(
+    "key"  varchar(255) NOT NULL,
+    "next" int8         NOT NULL,
+    PRIMARY KEY ("key")
 );
 COMMENT ON COLUMN "sequence"."key" IS '序号 Key';
 COMMENT ON COLUMN "sequence"."next" IS '下一可用序号';
 
-CREATE TABLE "solution" (
-  "exam_id" int8 NOT NULL,
-  "question_id" int8 NOT NULL,
-  "solution_text" text NOT NULL,
-  PRIMARY KEY ("exam_id", "question_id")
+CREATE TABLE "solution"
+(
+    "exam_id"       int8 NOT NULL,
+    "question_id"   int8 NOT NULL,
+    "solution_text" text NOT NULL,
+    PRIMARY KEY ("exam_id", "question_id")
 );
 COMMENT ON COLUMN "solution"."exam_id" IS '考试 ID';
 COMMENT ON COLUMN "solution"."question_id" IS '题目 ID';
 COMMENT ON COLUMN "solution"."solution_text" IS '解析内容';
 
-CREATE TABLE "swipe" (
-  "id" int8 NOT NULL,
-  "name" varchar(255) NOT NULL,
-  "status" int4 NOT NULL,
-  "image_id" int8 NOT NULL,
-  PRIMARY KEY ("id")
+CREATE TABLE "swipe"
+(
+    "id"       int8         NOT NULL,
+    "name"     varchar(255) NOT NULL,
+    "status"   int4         NOT NULL,
+    "image_id" int8         NOT NULL,
+    PRIMARY KEY ("id")
 );
 COMMENT ON COLUMN "swipe"."id" IS '轮播图 ID';
 COMMENT ON COLUMN "swipe"."name" IS '图片名称';
@@ -172,20 +193,25 @@ COMMENT ON COLUMN "swipe"."status" IS '轮播图状态';
 COMMENT ON COLUMN "swipe"."image_id" IS '图片 URL';
 COMMENT ON TABLE "swipe" IS '轮播图';
 
-CREATE TABLE "user" (
-  "id" int8 NOT NULL,
-  "open_id" char(28) COLLATE "pg_catalog"."default",
-  "username" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
-  "password" varchar(255),
-  "phone_number" char(11) COLLATE "pg_catalog"."default",
-  "avatar_id" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
-  "non_locked" bool NOT NULL DEFAULT false,
-  CONSTRAINT "user_pkey" PRIMARY KEY ("id")
+CREATE TABLE "user"
+(
+    "id"           int8                                        NOT NULL,
+    "open_id"      char(28) COLLATE "pg_catalog"."default",
+    "username"     varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
+    "password"     varchar(255),
+    "phone_number" char(11) COLLATE "pg_catalog"."default",
+    "avatar_id"    varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
+    "non_locked"   bool                                        NOT NULL DEFAULT false,
+    CONSTRAINT "user_pkey" PRIMARY KEY ("id")
 );
-ALTER TABLE "user" OWNER TO "postgres";
+ALTER TABLE "user"
+    OWNER TO "postgres";
 CREATE UNIQUE INDEX "user_open_id_uindex" ON "user" USING btree (
-  "open_id" COLLATE "pg_catalog"."default" "pg_catalog"."bpchar_ops" ASC NULLS LAST
-);
+    "open_id"
+    COLLATE "pg_catalog"."default"
+    "pg_catalog"."bpchar_ops" ASC NULLS
+    LAST
+    );
 COMMENT ON COLUMN "user"."id" IS '用户 ID';
 COMMENT ON COLUMN "user"."open_id" IS '用户微信 ID';
 COMMENT ON COLUMN "user"."username" IS '用户名';
@@ -194,12 +220,13 @@ COMMENT ON COLUMN "user"."phone_number" IS '用户联系电话';
 COMMENT ON COLUMN "user"."avatar_id" IS '用户头像附件 ID';
 COMMENT ON COLUMN "user"."non_locked" IS '用户是否被封禁';
 
-CREATE TABLE "vacancy" (
-  "id" int8 NOT NULL,
-  "name" varchar(255) NOT NULL,
-  "province" char(2) NOT NULL,
-  "prefecture" char(4) NOT NULL,
-  PRIMARY KEY ("id")
+CREATE TABLE "vacancy"
+(
+    "id"         int8         NOT NULL,
+    "name"       varchar(255) NOT NULL,
+    "province"   char(2)      NOT NULL,
+    "prefecture" char(4)      NOT NULL,
+    PRIMARY KEY ("id")
 );
 COMMENT ON COLUMN "vacancy"."id" IS '岗位 ID';
 COMMENT ON COLUMN "vacancy"."name" IS '岗位名称';
